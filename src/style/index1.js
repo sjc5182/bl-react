852f7be4ede6ea2896fd030f69bd848d6be4e3e5
John Chen <shiguangchen1987@gmail.com>
2018-04-18 12:42:00 -0700

    Add files via upload

---
+++ src/style/index1.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const ApplicationStyle = styled.div`
+  display:flex;
+  flex-direction:column;
+  height:100%;
+  .app-main-content-container{
+    display:flex;
+    height:100%;
+  }
+`
