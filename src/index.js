c066424629cf8d77b4e3aafee9c47ef29a889a1e
John Chen <shiguangchen1987@gmail.com>
2018-04-18 12:42:20 -0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ApolloClient, createNetworkInterface, ApolloProvider } from 'react-apollo';
+import 'semantic-ui-css/semantic.min.css';
+import './globals/globals.css';
+
+import Routes from './routes';
+import registerServiceWorker from './registerServiceWorker';
+
+const networkInterface = createNetworkInterface({
+  uri: 'http://localhost:4000/graphql',
+});
+
+const client = new ApolloClient({
+  networkInterface,
+});
+
+const App = (
+  <ApolloProvider client={client}>
+    <Routes />
+  </ApolloProvider>
+);
+
+ReactDOM.render(App, document.getElementById('root'));
+registerServiceWorker();
